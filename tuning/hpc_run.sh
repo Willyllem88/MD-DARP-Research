cb586ec1409e20a0177f936fbb983a7d65af2fef
guillem.cabre <guillemcabre2004@gmail.com>
2026-08-01 10:11:58 +0200

    FIX

--- tuning/hpc_run.sh
+++ tuning/hpc_run.sh
@@ -2,13 +2,13 @@
 #SBATCH --job-name=DARPMD-IRACE                         # Job name
 #SBATCH --output=darpmd_irace_%j.out                    # Standard output
 #SBATCH --error=darpmd_irace_%j.err                     # Standard error
-#SBATCH --time=6:00:00                                  # Maximum time (hh:mm:ss)
+#SBATCH --time=24:00:00                                 # Maximum time (hh:mm:ss)
 #SBATCH --mail-user guillem.cabre@estudiantat.upc.edu   # Mail recipient
 #SBATCH --mail-type=ALL                                 # Mail events (BEGIN, END, FAIL, ALL)
 #SBATCH --partition=short                               # Partition/queue, change if your cluster has a different one
 #SBATCH --ntasks=1                                      # Number of tasks
 #SBATCH --cpus-per-task=20                              # CPUs per task
-#SBATCH --mem=128G                                       # Memory per node
+#SBATCH --mem=128G                                      # Memory per node
 
 # Activar conda
 source /home/soft/anaconda3/etc/profile.d/conda.sh
